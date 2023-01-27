d731c723bd39d0c2fa2f36dba17c0fca8b5a519b
Toni Haryanto <toha.samba@gmail.com>
2023-01-27 17:21:18 +0700

    Initial commit

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -8,4 +8,34 @@ class Welcome extends CI_Controller {
 		$git = new CzProject\GitPhp\Git;
 		$repo = $git->cloneRepository('git@github.com:yllumi/nyoba.git', '/var/www/html/nyoba');
 	}
+
+	public function commit()
+	{
+		$git = new CzProject\GitPhp\Git;
+		$repo = $git->open('/var/www/html/nyoba');
+        if($repo->hasChanges()){
+            $repo->addAllChanges();
+            $repo->commit('Commit '.date("d F Y H:i:s"));
+            echo "commited.";
+        } else
+            echo "nothing to commit.";
+	}
+
+	public function push()
+	{
+		$git = new CzProject\GitPhp\Git;
+		$repo = $git->open('/var/www/html/nyoba');
+		$repo->push(['origin', 'main']);
+	}
+
+	public function config()
+	{
+		$git = new CzProject\GitPhp\Git;
+		$repo = $git->open('/var/www/html/nyoba');
+
+		print_r($repo->execute('log'));
+		print_r($repo->execute('config', '--global', 'user.name'));
+		print_r($repo->execute('config', '--global', 'user.email'));
+	}
+
 }
